60edb5fb6369c896e1878e44e0cdc7562c4c1573
Caleb Froese <caleb.froese@gmail.com>
2018-09-01 14:53:50 +0930

    Add response manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LexRuntime } from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,40 @@ import { LexRuntime } from 'aws-sdk';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  lex: LexRuntime;
+  lex: AWS.LexRuntime;
   response = '';
   input = '';
+  sessionAttributes: any = {};
 
   ngOnInit() {
-    this.lex = new LexRuntime();
+    // Initialize the Amazon Cognito credentials provider
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:faef712c-14c6-4493-b3d7-3e3bd631bf03'
+    });
+    this.lex = new AWS.LexRuntime();
+  }
+
+  chat(message: string) {
+    this.lex.postText(
+      {
+        botAlias: '$LATEST',
+        botName: 'ProductOrdering',
+        inputText: message,
+        userId: 'chatbot-lex' + Date.now(),
+        sessionAttributes: this.sessionAttributes
+      },
+      (err, data) => {
+        if (data) {
+          this.sessionAttributes = data.sessionAttributes;
+          console.log(this.sessionAttributes);
+          this.addResponse(data);
+        }
+      }
+    );
+  }
+
+  addResponse(response: any) {
+    this.response = response.message;
   }
 }
