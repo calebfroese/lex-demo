87b154796856b78c33bd3b962f7f6407dfd30151
Caleb Froese <caleb.froese@gmail.com>
2018-09-01 14:40:16 +0930

    Add AWS sdk typings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LexRuntime } from 'aws-sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lex-demo';
+export class AppComponent implements OnInit {
+  lex: LexRuntime;
+  response = '';
+  input = '';
+
+  ngOnInit() {
+    this.lex = new LexRuntime();
+  }
 }
